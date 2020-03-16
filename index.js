f9fbb359c06b5ba4a9690ea1b18bb083e6f3dd22
thanhlong <info@coders.tokyo>
2020-03-16 09:28:26 +0700

    finsh project

---
+++ index.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+
+let electronicUrl = 'https://nshopvn.com/';
+(async () => {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto(electronicUrl);
+
+  // get nshop details
+  let electronicData = await page.evaluate(() => {
+    let products = [];
+    let product_wrapper = document.querySelectorAll('.product-wrapper');
+    product_wrapper.forEach((product) => {
+      let dataJson = {};
+      try {
+        dataJson.img = product.querySelector('.image > img').src;
+        dataJson.title = product.querySelector('.woocommerce-loop-product__title').innerText;
+        dataJson.price = product.querySelector('.price').innerText;
+      }
+      catch (err) {
+          console.log(err)
+      }
+      products.push(dataJson);
+    });
+    return products;
+  });
+
+   console.log(electronicData);
+})();
